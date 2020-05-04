d0751ab77da69083121a2cc6d991c4a0bdbf6949
LucaBrasi89 <andrew.sotnikov@zoho.com>
2020-05-04 23:15:37 +0300

    initial

---
+++ WpfApp1/Backuper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace ZipService
+{
+    class Backuper
+    {
+        public static void doBackup()
+        {
+            string profileDir = "xuhnyq1s.default-release";
+
+            string appData = Environment.GetEnvironmentVariable("APPDATA");
+            string path = Path.Combine(appData, @"Mozilla\Firefox\Profiles\");
+
+            string source = Path.Combine(path, profileDir);
+            string dest = Path.Combine(path, $"{profileDir}.zip");
+
+            try
+            {
+                ZipFile.CreateFromDirectory(source, dest);
+            }
+            catch (IOException e)
+            {
+                throw new System.IO.IOException();
+            }
+
+
+
+            }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZipService;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Backuper.doBackup();
+                MessageBox.Show("Complete :)", "Hey!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            catch (IOException)
+            {
+                MessageBox.Show("Probably, Firefox is already ran.\n Please close FF and try again...", "Hey!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
